f41b44d4c274d03c1f6543da364a93c28aa52a6a
sdwck <sdwcktarakanov@gmail.com>
2025-09-10 12:56:38 +0300

    feat: add health controller

---
+++ src/SolarPanel.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolarPanel.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    [HttpHead]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "Healthy", timestamp = DateTime.UtcNow });
+    }
+}
